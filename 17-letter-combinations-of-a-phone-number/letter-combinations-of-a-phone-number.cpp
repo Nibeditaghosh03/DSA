f770680fd4d5e0c56bf9d0b7bd468f7297a0d735
Nibeditaghosh03 <nibeditag452@gmail.com>
2026-07-11 01:17:43 +0530

    Time: 3 ms (5.52%) | Memory: 9.3 MB (62.21%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,33 +1,31 @@
-class Solution {
-private:
-    void solve(string digit, string output, int i, vector<string>& ans,
-               string mapping[]) {
-        if (i >= digit.length()) {
-            ans.push_back(output);
-            return;
+class Solution{
+     private:
+        void solve(string digit, string output, int index, vector<string>& ans, string mapping[]){
+
+        if(index >= digit.length()){
+           ans.push_back(output);
+           return;
         }
+        int number = digit[index] - '0';
+        string value = mapping[number];
 
-        int num = digit[i] - '0';
-        string value = mapping[num];
-
-        for (int j = 0; j < value.length(); j++) {
-            output.push_back(value[j]);
-            solve(digit, output, i + 1, ans, mapping);
+        for(int i = 0; i < value.length(); i++){
+            output.push_back(value[i]);
+            solve(digit, output, index+1, ans, mapping);
             output.pop_back();
         }
-    }
-
-public:
-    vector<string> letterCombinations(string digit) {
-        vector<string> ans;
-        if (digit.length() == 0) {
-            return ans;
         }
-        int i = 0;
-        string output;
-        string mapping[10] = {"",    "",    "abc",  "def", "ghi",
-                              "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        solve(digit, output, i, ans, mapping);
+    public:
+    vector<string> letterCombinations(string digit){
+        vector<string> ans;
+        if(digit.length() == 0){
+        return ans;
+        }
+        string output = "";
+        int index = 0;
+
+        string mapping[10] = {"","", "abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        solve(digit, output, index, ans, mapping);
         return ans;
     }
 };
