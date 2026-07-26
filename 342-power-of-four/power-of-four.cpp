dced8d7f98feae8b30145f11b206f2ac94a291ea
Nibeditaghosh03 <nibeditag452@gmail.com>
2026-07-26 20:05:35 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (81.18%) - LeetSync

---
+++ 342-power-of-four/power-of-four.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        while(n > 1 && n % 4 == 0) {
+            n = n/4;
+        }
+        if(n == 1) {
+            return true;
+        }
+        return false;
+    }
+};
