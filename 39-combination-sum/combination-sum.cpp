4f6a998507f98f613c8d10a2ac7435d5bfefdeba
Nibeditaghosh03 <nibeditag452@gmail.com>
2026-07-28 23:08:39 +0530

    Time: 2 ms (58.61%) | Memory: 14.1 MB (59.31%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,27 +1,68 @@
 class Solution {
-    public:
-    void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int>&ds)
-    {
-     if(ind == arr.size()){
-        if(target == 0){
-            ans.push_back(ds);
+public:
+
+    void findCombination(
+        int ind,
+        vector<int>& arr,
+        int target,
+        vector<vector<int>>& ans,
+        vector<int>& ds
+    ) {
+
+        if(ind == arr.size()) {
+
+            if(target == 0) {
+                ans.push_back(ds);
+            }
+
+            return;
         }
-        return;
-    }
-       
+
+
+        // PICK
         if(arr[ind] <= target) {
+
             ds.push_back(arr[ind]);
-            findCombination(ind, target - arr[ind], arr, ans, ds);
+
+            findCombination(
+                ind,
+                arr,
+                target - arr[ind],
+                ans,
+                ds
+            );
+
             ds.pop_back();
         }
-        findCombination(ind+1, target, arr, ans, ds);
 
+
+        // NOT PICK
+        findCombination(
+            ind + 1,
+            arr,
+            target,
+            ans,
+            ds
+        );
     }
-public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+
+
+    vector<vector<int>> combinationSum(
+        vector<int>& candidates,
+        int target
+    ) {
+
         vector<vector<int>> ans;
         vector<int> ds;
-        findCombination(0, target, candidates, ans, ds);
+
+        findCombination(
+            0,
+            candidates,
+            target,
+            ans,
+            ds
+        );
+
         return ans;
     }
 };
