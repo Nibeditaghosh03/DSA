8cc8fabb03c1522d81d77a062490c27da2fddbfe
Nibeditaghosh03 <nibeditag452@gmail.com>
2026-08-13 02:24:30 +0530

    Time: 55 ms (90.76%) | Memory: 149.3 MB (84.13%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -6,15 +6,23 @@ public:
         int i = 0;
         int j = 0; 
         int result = 0;
+        int culprit = 0;
 
         while(j < n){
             mp[nums[j]]++;
-
-            while(i < j && mp[nums[j]] > k) {
+            if(mp[nums[j]] == k+1) {
+                culprit ++;
+            }
+            if(culprit > 0) {
                 mp[nums[i]]--;
+                if(mp[nums[i]] == k) {
+                    culprit--;
+                }
                 i++;
             }
-            result = max(result, j-i+1);
+            if(culprit == 0) {
+                result = max(result, j-i+1);
+            }
             j++;
         }
         return result;
